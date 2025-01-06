ef462b76969de5db0992b9cc65c682f11af0132e
Fukuro <anya.androschuk@gmail.com>
2025-01-06 18:46:26 +0200

    test

--- src/commands/reply.command.js
+++ src/commands/reply.command.js
@@ -14,9 +14,8 @@ export const replyCommand = (async (context) => {
         }
 
         const receiver = context.message.text.split('@')[1];
-        const receiverId = await bot.telegram.getChat(`@${receiver}`);
-
         await context.reply(`Next messages will be set to ${receiver}`);
+        const receiverId = await bot.telegram.getChat(`@${receiver}`);
 
         await context.scene.enter('DIALOG_SCENE', {
             userId: receiverId.id,
